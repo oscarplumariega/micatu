2a4114e95150738ee556757401b993df749af515
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-06-10 00:16:26 +0200

    toolbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataListComponent } from './src/components/data-list/data-list.component';
+import { ToolbarComponent } from './src/components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DataListComponent, RouterOutlet],
+  imports: [DataListComponent, ToolbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
