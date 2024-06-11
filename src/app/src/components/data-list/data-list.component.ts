bc82c11aa4b504e3f4ea6391f47bd707555e3a31
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-06-12 00:14:39 +0200

    pagination functionality

--- src/app/src/components/data-list/data-list.component.ts
+++ src/app/src/components/data-list/data-list.component.ts
@@ -4,7 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MicatuService } from '../../../core/services/micatu.service';
 import { AsyncPipe } from '@angular/common';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-data-list',
@@ -16,6 +16,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 export class DataListComponent {
   salesData: any;
   records!: number;
+  recordsPerPage = 8;
+  pagedList: any= [];
   constructor(private service: MicatuService) { }
 
   ngOnInit() {
@@ -26,6 +28,17 @@ export class DataListComponent {
     this.service.getSalesAds().subscribe(response => {
       this.salesData = response;
       this.records = this.salesData.length;
+      this.pagedList = this.salesData.slice(0, this.recordsPerPage);
     });
   }
+
+  OnPageChange(event: PageEvent){
+    let startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if(endIndex > this.records){
+      endIndex = this.records;
+    }
+    this.pagedList = this.salesData.slice(startIndex, endIndex);
+  }
+
 }
