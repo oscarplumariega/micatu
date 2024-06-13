1d330247973f5c393631cdb270453959b0da1e02
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-06-14 00:18:44 +0200

    pagination

--- src/app/src/components/data-list/data-list.component.ts
+++ src/app/src/components/data-list/data-list.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { MicatuService } from '../../../core/services/micatu.service';
 import { AsyncPipe } from '@angular/common';
+import { DUIPaginator  } from "david-ui-angular";
 
 @Component({
   selector: 'app-data-list',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, DUIPaginator],
   templateUrl: './data-list.component.html',
   styleUrl: './data-list.component.css'
 })
 export class DataListComponent {
   salesData: any;
-  records!: number;
+  records!: any;
   pageRecords!: any;
   recordsPerPage = 10;
   totalPages!: any;
+  numberOfPages: any = [];
   constructor(private service: MicatuService) { }
 
   ngOnInit() {
@@ -26,8 +28,18 @@ export class DataListComponent {
       this.salesData = response;
       this.records = this.salesData.length;
       this.pageRecords = this.salesData.slice(0, this.recordsPerPage); 
-      this.totalPages = this.salesData / this.recordsPerPage;
+      this.totalPages = this.records / this.recordsPerPage;
+
+      for (let i = 1; i <= this.totalPages; i++) {
+        this.numberOfPages.push(i);
+      } 
     });
   }
+
+  goToPage(nextPage: number){
+    this.pageRecords = this.salesData.slice((nextPage - 1) * this.recordsPerPage, nextPage * this.recordsPerPage);
+  }
+
+
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}",
+    "./node_modules/david-ui-angular/**/*.{html,ts,js,mjs}"
   ],
   theme: {
     extend: {},
