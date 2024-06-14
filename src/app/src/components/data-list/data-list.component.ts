340f5668ee3dea3d740ee3140690ffc46185185f
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-06-14 23:11:26 +0200

    activePage pagination style

--- src/app/src/components/data-list/data-list.component.ts
+++ src/app/src/components/data-list/data-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MicatuService } from '../../../core/services/micatu.service';
 import { AsyncPipe } from '@angular/common';
-import { DUIPaginator  } from "david-ui-angular";
+import { DUIPaginator } from "david-ui-angular";
 
 @Component({
   selector: 'app-data-list',
@@ -10,36 +10,56 @@ import { DUIPaginator  } from "david-ui-angular";
   templateUrl: './data-list.component.html',
   styleUrl: './data-list.component.css'
 })
-export class DataListComponent {
+export class DataListComponent{
   salesData: any;
   records!: any;
   pageRecords!: any;
   recordsPerPage = 10;
   totalPages!: any;
   numberOfPages: any = [];
+
   constructor(private service: MicatuService) { }
 
   ngOnInit() {
     this.getSalesAds();
+
+    window.onload = (event) => {
+      let el = document.getElementById("1");
+      if (el !== null) {
+        el.style.backgroundColor = "#007bff";
+      }
+    };
   }
 
-  getSalesAds(){
+  getSalesAds() {
     this.service.getSalesAds().subscribe(response => {
       this.salesData = response;
       this.records = this.salesData.length;
-      this.pageRecords = this.salesData.slice(0, this.recordsPerPage); 
+      this.pageRecords = this.salesData.slice(0, this.recordsPerPage);
       this.totalPages = this.records / this.recordsPerPage;
 
       for (let i = 1; i <= this.totalPages; i++) {
         this.numberOfPages.push(i);
-      } 
+      }
     });
   }
 
-  goToPage(nextPage: number){
+  goToPage(nextPage: number) {
     this.pageRecords = this.salesData.slice((nextPage - 1) * this.recordsPerPage, nextPage * this.recordsPerPage);
+    for (let i = 0; i <= this.totalPages; i++) {
+      let el = document.getElementById(`${i}`);
+      if (el !== null) {
+        el.style.backgroundColor = "rgb(31,41,55)";
+
+        if (i == nextPage) {
+          let el = document.getElementById(`${nextPage}`);
+          if (el !== null) {
+            el.style.backgroundColor = "#007bff";
+          }
+        }
+      }
+    }
+    document.body.scrollIntoView();
   }
-
-
 
 }
